e75f126d8191bfef4e0fee4270ab9662c177e08b
Alban Bailly <alban@zivtech.com>
2013-07-03 11:26:41 -0400

    Initial Commit

---
+++ README.txt
@@ -0,0 +1,25 @@
+
+The Responsive Navigation module integrates the mean-menu.js
+plugin into Drupal.
+
+
+Features
+------------
+
+A simple module for a great responsive navigation solution. Full UI and supports expandable navigation children.
+
+
+Known Issues
+------------
+
+Needs Jquery Update as the script requires 1.8 or above.
+
+Author
+------------
+
+Alioso at Zivtech
+
+Thanks
+------------
+
+The folks at mean themes for such great plugin. 
---
+++ inline.js
@@ -0,0 +1,24 @@
+(function ($) {
+          Drupal.behaviors.responsive_menu = {
+              attach: function (context, settings) {
+                var menuclass = Drupal.settings.responsive_menu.menu_class;
+                $('.'+menuclass).meanmenu({
+                meanMenuClass:Drupal.settings.responsive_menu.menu_class,
+                meanMenuClose:Drupal.settings.responsive_menu.close_sign,
+                meanMenuCloseSize:Drupal.settings.responsive_menu.close_size,
+                //meanMenuOpen:Drupal.settings.responsive_menu.menu_open,
+                meanRevealPosition:Drupal.settings.responsive_menu.menu_position,
+                meanRevealPositionDistance:Drupal.settings.responsive_menu.menu_position_distance,
+                meanRevealColour:Drupal.settings.responsive_menu.menu_background,
+                meanRevealHoverColour:Drupal.settings.responsive_menu.menu_background_hover,
+                meanScreenWidth:Drupal.settings.responsive_menu.min_width,
+                meanNavPush:Drupal.settings.responsive_menu.nav_push,
+                meanShowChildren:Drupal.settings.responsive_menu.show_children,
+                meanExpandableChildren:Drupal.settings.responsive_menu.show_children_collapse,
+                meanExpand:Drupal.settings.responsive_menu.expand_sign,
+                meanContract:Drupal.settings.responsive_menu.contract_sign,
+                meanRemoveAttrs:Drupal.settings.responsive_menu.remove_att,
+              });
+             }
+         };
+        })(jQuery);
